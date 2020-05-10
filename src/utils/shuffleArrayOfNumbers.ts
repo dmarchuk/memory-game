9e57f7a581bcd2efe583a2c8939d2186ec9273d2
Daniel Marchuk <dmarchuk@me.com>
2020-05-11 00:45:02 +0200

    Add comments to shuffleArrayOfNumbers.ts

--- src/utils/shuffleArrayOfNumbers.ts
+++ src/utils/shuffleArrayOfNumbers.ts
@@ -1,4 +1,6 @@
+// Shuffles an array of numbers, basically the same as https://stackoverflow.com/a/46545530/5263525
+
 export const shuffleArrayOfNumbers = (array: number[]) => array
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1]);
+    .map(a => [Math.random(), a]) // Add random number so we can sort by it
+    .sort((a, b) => a[0] - b[0]) // Sort by the random number
+    .map(a => a[1]); // Remove the random number and keep only the original array members
