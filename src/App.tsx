bf3bfc606e0421e5bd430b808ca47588faf63e76
Daniel Marchuk <dmarchuk@me.com>
2020-05-10 15:47:33 +0200

    Add GlobalStyle and Board components to main App component + add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Board, GlobalStyle } from './components';
 
 export const App = () => (
     <div>
-        <header>
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
+        <GlobalStyle />
+        <Heading>Memory game</Heading>
+        <ContainerElement>
+            <Board numberOfCards={8} />
+        </ContainerElement>
     </div>
 );
+
+export const Heading = styled.h1`
+    text-align: center;
+`;
+
+export const ContainerElement = styled.div`
+    display: flex;
+    justify-content: center;
+`;
