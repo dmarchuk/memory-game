2444efcee86ec4dc8447a37251deda68563b3d9d
Daniel Marchuk <dmarchuk@me.com>
2020-05-10 15:46:17 +0200

    Add Board component + Re-export of all components in index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Board } from './Board/Board';
+export { Card } from './Card/Card';
+export { GlobalStyle } from './GlobalStyle/GlobalStyle';
+export { SuccessMessage } from './SuccessMessage/SuccessMessage';
